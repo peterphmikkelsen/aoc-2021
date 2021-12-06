955f765517ad200aecbe85e89b9117b91a043e54
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2021-12-06 19:15:54 +0100

    Update Utils.kt

--- src/Utils.kt
+++ src/Utils.kt
@@ -9,7 +9,20 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 fun readInputAsIntegers(name: String) = readInput(name).map { it.toInt() }
 
+fun readSingleLineInput(name: String) = File("src", "$name.txt").bufferedReader().readLine()
+
 /**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+inline fun <T> MutableList<T>.mapInPlace(mutator: (T)->T) {
+    val iterate = this.listIterator()
+    while (iterate.hasNext()) {
+        val oldValue = iterate.next()
+        val newValue = mutator(oldValue)
+        if (newValue !== oldValue) {
+            iterate.set(newValue)
+        }
+    }
+}
